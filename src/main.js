8c93ea826e9fb906348a2ac31c246f03c94fb236
SimonePizziconi <simonepizziconi05@gmail.com>
2024-12-16 11:19:02 +0100

    build: create project files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-// Importa il router
-import router from './router'
-
+// // Importa il router
+// import router from './router'
 
 // Importa scss
-import "./assets/styles/general.scss"
+import './assets/styles/general.scss';
 
-
-createApp(App).mount('#app')
+createApp(App).mount('#app');
 App.use(router);
