61eab48a7176ebfeba96279fb7eb8b04641261d7
SimonePizziconi <simonepizziconi05@gmail.com>
2024-12-16 11:49:45 +0100

    feat: create layout jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Header />
+  <Jumbo />
 </template>
 
 <script>
   import Header from '@/components/Header.vue';
+  import Jumbo from '@/components/Jumbo.vue';
   export default {
     components: {
       Header,
+      Jumbo,
     },
   };
 </script>
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="Jumbo">
+    <div class="flex-container">
+      <div class="current-weather">Gradi Attuale</div>
+      <div class="icon-weather">Icona Meteo</div>
+    </div>
+    <div class="city">Città</div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .Jumbo {
+    // DEBUG
+    height: 500px;
+    width: 100%;
+    background-color: aqua;
+
+    .flex-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 70%;
+      margin: 0 auto;
+
+      .current-weather {
+        // DEBUG
+        width: 100px;
+        height: 100px;
+        background-color: bisque;
+      }
+
+      .icon-weather {
+        // DEBUG
+        width: 100px;
+        height: 100px;
+        background-color: bisque;
+      }
+    }
+
+    .city {
+      text-align: center;
+    }
+  }
+</style>
